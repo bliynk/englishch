9d3a7bec0c03faae4c75f7ed16e57eb127b12b7b
Hamza <hamzazulqarnain.47@gmail.com>
2022-07-25 15:57:04 +0500

    recoil | notification and atoms added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import LoginPage from "./Components/LoginPage/LoginPage";
 import Customers from "./Components/Customers/Customers";
+import {ToastContainer} from "react-toastify";
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" extract element={<ImportAllComponents />} />
-          <Route path="/loginpage" element={<LoginPage />} />
-          <Route path="/customers" element={<Customers />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" extract element={<ImportAllComponents />} />
+                    <Route path="/loginpage" element={<LoginPage />} />
+                    <Route path="/customers" element={<Customers />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Routes>
+            </BrowserRouter>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </div>
+    );
 }
 
 export default App;
